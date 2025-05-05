cf5327b8772799a1682a7fc2bf3dad2bc936d81f
Anna Tiufiakova <annavtiufiakova@gmail.com>
2025-05-05 11:51:06 -0500

    Update ingredients page to display and search CSV data

--- pages/ingredients.py
+++ pages/ingredients.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from utils.session import go_to
 
 
@@ -7,3 +8,20 @@ def render():
     if st.button("🔙 Go to home page"):
         go_to("home")
         st.rerun()
+
+    # Load csv data
+    try:
+        df = pd.read_csv("data/item_master.csv", encoding="latin1")
+        # Searching fiel
+        search_field = st.text_input("Search ingredient by name:")
+        if search_field:
+            filtered_df = df[
+                df["name"].str.contains(search_field, case=False, na=False)
+            ]
+        else:
+            filtered_df = df
+        st.dataframe(filtered_df.reset_index(drop=True), use_container_width=True)
+    except FileNotFoundError:
+        st.error("The file 'item_master.csv' was not found.")
+    except Exception as e:
+        st.error(f"An error occurred while loading the data: {e}")
